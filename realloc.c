f42d4102993502fcf98add43439b101e1741ee4b
frenol021 <frenol021@gmail.com>
2022-09-15 06:56:07 -0700

    updated

--- realloc.c
+++ realloc.c
@@ -1,57 +1,62 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: input pointer
- * @old_size: size of old ptr
- * @new_size: size of new ptr
- * Return: reallocated ptr
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+char *_memset(char *s, char b, unsigned int n)
 {
-	void *res = NULL;
+	unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (!ptr)
-	{
-		free(ptr);
-		res = malloc(new_size);
-		if (!res)
-		{
-			perror("Malloc failed");
-			exit(errno);
-		}
-		return (res);
-	}
-	if (!new_size && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	res = malloc(new_size);
-	if (!res)
-	{
-		perror("Malloc failed");
-		exit(errno);
-	}
-	_memcpy(res, ptr, old_size);
-	free(ptr);
-	return (res);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
 
 /**
- * _memcpy - copies memory area
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to be copied
- * Return: pointer to dest
+ * ffree - frees a string of strings
+ * @pp: string of strings
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void ffree(char **pp)
 {
-	char *ptr = dest;
+	char **a = pp;
 
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to da ol'block nameen.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
 }
